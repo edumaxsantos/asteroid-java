d84610f43f93be83a66f1b8ae0a97d71a64d074f
Eduardo M. C. Santos <edumaxsantos@gmail.com>
2024-08-03 01:14:56 +0100

    Update how to handle events. Add star gold events.

--- core/src/main/java/com/github/edumaxsantos/drop/asteroid/MyContactListener.java
+++ core/src/main/java/com/github/edumaxsantos/drop/asteroid/MyContactListener.java
@@ -8,18 +8,9 @@ public class MyContactListener implements ContactListener {
         var bodyA = contact.getFixtureA().getBody();
         var bodyB = contact.getFixtureB().getBody();
 
-
-
-        if (bodyA.getUserData() instanceof Ship ship && bodyB.getUserData() instanceof Asteroid asteroid) {
-            ship.takeDamage(asteroid.getDamage());
-
-            if (ship.isDead()) {
-                ship.destroy();
-            }
-            asteroid.destroy();
-        }
-
-        missileHitAsteroid(bodyA, bodyB);
+        handleAsteroidAndShip(bodyA, bodyB);
+        handleMissileAndAsteroid(bodyA, bodyB);
+        handleMissileAndStarGold(bodyA, bodyB);
     }
 
     @Override
@@ -29,7 +20,6 @@ public class MyContactListener implements ContactListener {
 
     @Override
     public void preSolve(Contact contact, Manifold oldManifold) {
-        //System.out.println("Got preSolve");
     }
 
     @Override
@@ -37,23 +27,95 @@ public class MyContactListener implements ContactListener {
 
     }
 
-    private void missileHitAsteroid(Body bodyA, Body bodyB) {
-        if (bodyA.getUserData() instanceof Asteroid asteroid && bodyB.getUserData() instanceof Missile missile) {
-            asteroid.takeDamage(missile.getDamage());
-            if (asteroid.isDead()) {
-                asteroid.destroy();
-                ((Ship) missile.getParent()).screen.game.increasePoint();
-            }
+    private boolean isMissile(Body body) {
+        return body.getUserData() instanceof Missile;
+    }
 
-            missile.destroy();
-        }else if (bodyB.getUserData() instanceof Asteroid asteroid && bodyA.getUserData() instanceof Missile missile) {
-            asteroid.takeDamage(missile.getDamage());
-            if (asteroid.isDead()) {
-                asteroid.destroy();
-                ((Ship) missile.getParent()).screen.game.increasePoint();
-            }
+    private boolean isFlyingObject(Body body) {
+        return  body.getUserData() instanceof FlyingObject;
+    }
 
-            missile.destroy();
+    private boolean isStarGold(Body body) {
+        return body.getUserData() instanceof StarGold;
+    }
+
+    private boolean isShip(Body body) {
+        return body.getUserData() instanceof Ship;
+    }
+
+    private boolean isAsteroid(Body body) {
+        return body.getUserData() instanceof Asteroid;
+    }
+
+    private void handleAsteroidAndShip(Body bodyA, Body bodyB) {
+        if (isShip(bodyA) && isAsteroid(bodyB)) {
+            var ship = (Ship) bodyA.getUserData();
+            var asteroid = (Asteroid) bodyB.getUserData();
+
+            asteroidHitsShip(asteroid, ship);
+        } else if (isShip(bodyB) && isAsteroid(bodyA)) {
+            var ship = (Ship) bodyB.getUserData();
+            var asteroid = (Asteroid) bodyA.getUserData();
+
+            asteroidHitsShip(asteroid, ship);
         }
+    }
+
+    private void handleMissileAndAsteroid(Body bodyA, Body bodyB) {
+        if (isAsteroid(bodyA) && isMissile(bodyB)) {
+            var missile = (Missile) bodyB.getUserData();
+            var flyingObject = (FlyingObject) bodyA.getUserData();
+
+            missileHitFlyingObject(missile, flyingObject);
+        } else if (isAsteroid(bodyB) && isMissile(bodyA)) {
+            var missile = (Missile) bodyA.getUserData();
+            var flyingObject = (FlyingObject) bodyB.getUserData();
+
+            missileHitFlyingObject(missile, flyingObject);
+        }
+    }
+
+    private void handleMissileAndStarGold(Body bodyA, Body bodyB) {
+        if (isStarGold(bodyA) && isMissile(bodyB)) {
+            var starGold = (StarGold) bodyA.getUserData();
+            var missile = (Missile) bodyB.getUserData();
+
+            missileHitsStarGold(starGold, missile);
+        } else if (isStarGold(bodyB) && isMissile(bodyA)) {
+            var starGold = (StarGold) bodyB.getUserData();
+            var missile = (Missile) bodyA.getUserData();
+
+            missileHitsStarGold(starGold, missile);
+        }
+    }
+
+    private void missileHitsStarGold(StarGold starGold, Missile missile) {
+        starGold.takeDamage(missile.getDamage());
+
+        if (starGold.isDead()) {
+            starGold.destroy();
+            ((Ship) missile.getParent()).heal(25f);
+        }
+
+        missile.destroy();
+    }
+
+    private void asteroidHitsShip(Asteroid asteroid, Ship ship) {
+        ship.takeDamage(asteroid.getDamage());
+
+        if (ship.isDead()) {
+            ship.destroy();
+        }
+        asteroid.destroy();
+    }
+
+    private void missileHitFlyingObject(Missile missile, FlyingObject flyingObject) {
+        flyingObject.takeDamage(missile.getDamage());
+        if (flyingObject.isDead()) {
+            flyingObject.destroy();
+            ((Ship) missile.getParent()).screen.game.increasePoint();
+        }
+
+        missile.destroy();
     }
 }
