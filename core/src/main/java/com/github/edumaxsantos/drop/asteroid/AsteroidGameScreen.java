586e07277347cc4b66396a3c8789add03edb9b7c
Eduardo M. C. Santos <edumaxsantos@gmail.com>
2024-08-03 01:13:17 +0100

    Update to handle flying objects. Change how to handle generating things over time.

--- core/src/main/java/com/github/edumaxsantos/drop/asteroid/AsteroidGameScreen.java
+++ core/src/main/java/com/github/edumaxsantos/drop/asteroid/AsteroidGameScreen.java
@@ -4,20 +4,25 @@ import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.ScreenAdapter;
 import com.badlogic.gdx.graphics.Cursor;
 import com.badlogic.gdx.math.Vector2;
-import com.badlogic.gdx.physics.box2d.*;
+import com.badlogic.gdx.physics.box2d.BodyDef;
+import com.badlogic.gdx.physics.box2d.Box2D;
+import com.badlogic.gdx.physics.box2d.Box2DDebugRenderer;
+import com.badlogic.gdx.physics.box2d.World;
 import com.badlogic.gdx.scenes.scene2d.Stage;
 import com.badlogic.gdx.utils.Array;
 import com.badlogic.gdx.utils.ScreenUtils;
-import com.badlogic.gdx.utils.TimeUtils;
+import com.badlogic.gdx.utils.Timer;
 import com.badlogic.gdx.utils.viewport.ScreenViewport;
 
 import java.util.concurrent.TimeUnit;
 
+import static com.github.edumaxsantos.drop.asteroid.BodyFactory.createBox;
+import static com.github.edumaxsantos.drop.asteroid.BodyFactory.createCircle;
+
 public class AsteroidGameScreen extends ScreenAdapter {
 
     public final AsteroidGame game;
-    private final Array<Asteroid> asteroids;
-    long lastAsteroidTime;
+    private final Array<FlyingObject> flyingObjects;
     private final Ship ship;
     private final Stage stage;
     private final Box2DDebugRenderer debugRenderer;
@@ -29,13 +34,13 @@ public class AsteroidGameScreen extends ScreenAdapter {
         Box2D.init();
         this.stage = new Stage(new ScreenViewport());
 
-        this.asteroids = new Array<>();
+        this.flyingObjects = new Array<>();
         this.world = new World(new Vector2(0, 0), true);
 
         var shipX = Ship.getRealWidth();
         var shipY = Ship.getRealHeight();
 
-        var shipBody = createBox((float) Gdx.graphics.getWidth() / 2, (float) Gdx.graphics.getHeight() / 2, shipX, shipY, BodyDef.BodyType.DynamicBody);
+        var shipBody = createBox(world, (float) Gdx.graphics.getWidth() / 2, (float) Gdx.graphics.getHeight() / 2, shipX, shipY, BodyDef.BodyType.DynamicBody);
 
         ship = new Ship(this, shipBody);
 
@@ -48,6 +53,9 @@ public class AsteroidGameScreen extends ScreenAdapter {
 
         world.setContactListener(new MyContactListener());
 
+        handleAsteroids();
+        handleStarGolds();
+
     }
 
     @Override
@@ -56,7 +64,7 @@ public class AsteroidGameScreen extends ScreenAdapter {
         stage.getCamera().update();
 
         world.step(1 /60f, 6, 2);
-        //debugRenderer.render(world, stage.getCamera().combined);
+        debugRenderer.render(world, stage.getCamera().combined);
 
         stage.getBatch().setProjectionMatrix(stage.getCamera().combined);
         stage.getBatch().begin();
@@ -64,14 +72,19 @@ public class AsteroidGameScreen extends ScreenAdapter {
         ship.draw(stage.getBatch(), 1);
         this.game.font.draw(stage.getBatch(), "Hits: " + game.getPoints(), 5, Gdx.graphics.getHeight() - 5);
         this.game.font.draw(stage.getBatch(), "Health: " + ship.getHealthComponent().getCurrentHealth(), Gdx.graphics.getWidth() - 100, Gdx.graphics.getHeight() - 5);
-        handleAsteroids();
+
+
+        for (var flyingObject : flyingObjects) {
+            flyingObject.act(delta);
+            flyingObject.draw(stage.getBatch(), 1);
+        }
 
         stage.getBatch().end();
 
-        for (var asteroid : asteroids) {
+        for (var asteroid : flyingObjects) {
             if (asteroid.shouldBeDestroyed()) {
                 world.destroyBody(asteroid.getBody());
-                asteroids.removeValue(asteroid, false);
+                flyingObjects.removeValue(asteroid, false);
             }
         }
     }
@@ -80,43 +93,42 @@ public class AsteroidGameScreen extends ScreenAdapter {
     public void dispose() {
         world.dispose();
         stage.dispose();
-        asteroids.clear();
+        flyingObjects.clear();
         debugRenderer.dispose();
         game.dispose();
     }
 
-    public Body createBox(float x, float y, float width, float height, BodyDef.BodyType bodyType) {
-        var bodyDef = new BodyDef();
-        bodyDef.type = bodyType;
-        bodyDef.position.set(x, y);
+    private void handleStarGolds() {
 
-        var body = world.createBody(bodyDef);
-
-        var shape = new PolygonShape();
-        shape.setAsBox(width / 2, height / 2);
-
-        body.createFixture(shape, 1f);
-        shape.dispose();
-
-        return body;
+        Timer.schedule(new Timer.Task() {
+            @Override
+            public void run() {
+                spawnStarGold();
+            }
+        }, TimeUnit.SECONDS.toSeconds(10), TimeUnit.SECONDS.toSeconds(10));
     }
 
     private void handleAsteroids() {
-        if (TimeUtils.nanoTime() - lastAsteroidTime > TimeUnit.SECONDS.toNanos(1)) {
-            spawnAsteroid();
-        }
 
-        for (var asteroid : asteroids) {
-            asteroid.act(TimeUtils.nanoTime());
-            asteroid.draw(stage.getBatch(), 1);
-        }
+        Timer.schedule(new Timer.Task() {
+            @Override
+            public void run() {
+                spawnAsteroid();
+            }
+        }, TimeUnit.SECONDS.toSeconds(1), TimeUnit.SECONDS.toSeconds(1));
+    }
+
+    private void spawnStarGold() {
+        var starGoldBody = createCircle(world, 50, 50, StarGold.getRealWidth(), BodyDef.BodyType.DynamicBody);
+        var starGold = StarGold.random(starGoldBody);
+
+        flyingObjects.add(starGold);
     }
 
     private void spawnAsteroid() {
-        var asteroidBody = createBox(50, 50, Asteroid.getRealWidth(), Asteroid.getRealHeight(), BodyDef.BodyType.DynamicBody);
+        var asteroidBody = createCircle(world, 50, 50, Asteroid.getRealWidth(), BodyDef.BodyType.DynamicBody);
         var asteroid = Asteroid.random(asteroidBody);
 
-        asteroids.add(asteroid);
-        lastAsteroidTime = TimeUtils.nanoTime();
+        flyingObjects.add(asteroid);
     }
 }
